b6c9c7555c6647d6aa8d9e1ef727f0191453fb7a
kelvinra <13521005@mahasiswa.itb.ac.id>
2022-10-03 11:38:19 +0700

    update BicubicInterpolate.java

---
+++ src/function/BicubicInterpolate.java
@@ -0,0 +1,170 @@
+import java.io.*;
+import java.util.*;
+public class BicubicInterpolate {
+    public static  double[] bicubicinterpolation(double[] matriks) {
+        double [][] matrix2 = new double[16][16];
+        int K = 0;
+        for (int j= 0; j<4; j++) {
+            for (int i=0; i<4; i++){
+                matrix2[0][K]= Math.pow((-1), i) * Math.pow((-1), j); 
+                matrix2[1][K] = Math.pow((0), i) * Math.pow((-1), j); 
+                matrix2[2][K] = Math.pow((1), i) * Math.pow((-1), j); 
+                matrix2[3][K] = Math.pow((2), i) * Math.pow((-1), j);             
+                matrix2[4][K]= Math.pow((-1), i) * Math.pow((0), j); 
+                matrix2[5][K]= Math.pow((0), i) * Math.pow((0), j); 
+                matrix2[6][K]= Math.pow((1), i) * Math.pow((0), j); 
+                matrix2[7][K]= Math.pow((2), i) * Math.pow((0), j); 
+                matrix2[8][K]= Math.pow((-1), i) * Math.pow((1), j); 
+                matrix2[9][K]= Math.pow((0), i) * Math.pow((1), j); 
+                matrix2[10][K] = Math.pow((1), i) * Math.pow((1), j); 
+                matrix2[11][K] = Math.pow((2), i) * Math.pow((1), j); 
+                matrix2[12][K] = Math.pow((-1), i) * Math.pow((2), j); 
+                matrix2[13][K] = Math.pow((0), i) * Math.pow((2), j); 
+                matrix2[14][K] = Math.pow((1), i) * Math.pow((2), j); 
+                matrix2[15][K] = Math.pow((2), i) * Math.pow((2), j);
+                K++;
+            }
+        }
+        double[][] IdentityMat = new double[16][16];
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
+                if (i == j) {
+                    IdentityMat[i][j] = 1;
+                } else {
+                    IdentityMat[i][j] = 0;
+                }
+            }
+        }
+        double[] matriksa = new double[16];
+        ForwardOBE(matrix2, IdentityMat);
+        BackwardOBE(matrix2, IdentityMat);
+        matriksa = multiplyMatrix(IdentityMat, matriks);
+        return matriksa;
+      }
+  
+      static void PrintEselon(double[][] Mat, double[][] MatAns) {
+        int ROW = Mat.length;
+        int COL = Mat[0].length;
+        System.out.println("Hasil matriks setelah di-REDUKSI: ");
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                System.out.printf("%.3f\t", Mat[i][j]);
+            }
+            System.out.print("  |\t");
+            for (int j = 0; j < COL; j++) {
+                System.out.printf("%.3f\t", MatAns[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static double[] reshape(double[][] matrix, int row) {
+        double[] matrixs = new double[row];
+        int x = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrixs[x] = matrix[i][j];
+                x++;
+            }
+        }
+        return matrixs;
+    }
+
+    static void ForwardOBE(double[][] Mat, double[][] MatAns) {
+        int ROW = Mat.length;
+        int COL = Mat[0].length;
+        for (int i = 0; i < ROW; i++) {
+            int Max = i;
+            for (int j = i + 1; j < ROW; j++) {
+                if (Math.abs(Mat[j][i]) > Math.abs(Mat[Max][i])) {
+                    Max = j;
+                    double[] Temp = Mat[i]; 
+                    Mat[i] = Mat[Max]; 
+                    Mat[Max] = Temp;
+                    double[] temp = MatAns[i]; 
+                    MatAns[i] = MatAns[Max]; 
+                    MatAns[Max] = temp;
+                    for (int k = i + 1; k < ROW; k++) {
+                        double Const = Mat[k][i] / Mat[i][i];
+                        for (int l = 0; l < COL; l++){
+                            Mat[k][l] -= Const * Mat[i][l];
+                            MatAns[k][l] -= Const * MatAns[i][l];
+                        }       
+                    }
+                }
+                else {
+                    for (int k = i + 1; k < ROW; k++) {
+                        double Const = Mat[k][i] / Mat[i][i];
+                        for (int l = 0; l < COL; l++){
+                            Mat[k][l] -= Const * Mat[i][l];
+                            MatAns[k][l] -= Const * MatAns[i][l];
+                        }       
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < ROW; i++) {
+            double Const1 = Mat[i][i];
+            for (int j = 0; j < COL; j++) {
+                Mat[i][j] = (Mat[i][j])/(Const1);
+                MatAns[i][j] = (MatAns[i][j])/(Const1);
+            }
+        }
+    }
+
+    static void BackwardOBE(double[][] Mat, double[][] MatAns) {
+        int ROW = Mat.length;
+        for (int i = ROW - 1; i >= 0; i--) {
+            for (int j = i - 1; j >= 0; j--) {
+                double Const = Mat[j][i] / Mat[i][i];
+                for (int k = ROW-1; k >= 0; k--) {
+                    Mat[j][k] -= Const * Mat[i][k];
+                    MatAns[j][k] -= Const * MatAns[i][k];
+                } 
+            }
+        }
+    }
+
+    static double[] multiplyMatrix(double[][] Mat, double[] MatAns) {
+        int ROW = Mat.length;
+        int COL = Mat[0].length;
+        double[] mOut = new double[ROW];
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                mOut[i] += Mat[i][j] * MatAns[j];
+            }
+        }
+        return mOut;
+    }
+    
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("PROGRAM INTERPOLASI BIKUBIK\n");
+        System.out.println("Masukkan elemen matriks (gunakan spasi sebagai pemisah dan enter untuk pindah baris): ");
+        double[][] matrix = new double[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                matrix[i][j] = input.nextDouble();
+            }
+        }
+        System.out.println();
+        double[] matrixs = new double[16];
+        matrixs = reshape(matrix,16);
+        double[] matriksa = new double[16];
+        matriksa =bicubicinterpolation(matrixs);
+        double count = 0;
+        System.out.print("Masukan Nilai X: ");
+        double x = input.nextDouble();
+        System.out.print("Masukan Nilai Y: ");
+        double y = input.nextDouble();
+        int k = 0;
+        for (int i=0;i<4;i++){
+            for (int j=0;j<4;j++){
+                count = count + matriksa[k]*Math.pow(x,i)*Math.pow(y,j);
+                k++;
+            }
+        }
+        System.out.println();
+        System.out.printf("f("+x+","+y+") = %.3f",count);
+    }
+}
